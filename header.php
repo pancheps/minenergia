b2b274c6eb66518b392246f3d4e1a714cf8a2adb
Pancho <pancho@localhost.localdomain>
2019-06-29 03:21:37 -0400

    Changed logo

--- header.php
+++ header.php
@@ -43,12 +43,7 @@ $currClass = $bgClasses[$classId];
 									<div class="app-logo-container">
 										<div class="app-title">
 											<a href="<?php echo site_url(); ?>" class="app-logo-link nuxt-link-exact-active nuxt-link-active">
-											<img src="<?php echo get_template_directory_uri() . '/img/bolivia.png'; ?>" class="app-logo-img" alt="">
-											<div class="app-logo">
-													<div class="app-logo-title">Estado Plurinacional de Bolivia</div>
-													<div class="app-logo-description"><?php bloginfo( 'name' ); ?></div>
-													<div class="app-logo-title">Estado Plurinacional de Bolivia</div>
-												</div>
+											<img src="<?php echo get_template_directory_uri() . '/img/logo1.png'; ?>" class="app-logo-img" alt="">
 											</a>
 										</div>
 									</div>
