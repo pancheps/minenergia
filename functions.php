88d368afdb4685ce3cb3b496d46624710ba9491f
Pancho <pancho@localhost.localdomain>
2019-07-15 03:31:50 -0400

    Slider with dots

--- functions.php
+++ functions.php
@@ -142,6 +142,7 @@ function endewp_scripts() {
 
 	wp_enqueue_script( 'endewp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	wp_enqueue_script( 'endewp-script-responsive', get_template_directory_uri() . '/js/responsivemenu.js', array(), '20151215', true );
+	wp_enqueue_script( 'endewp-script-jquery-min', get_template_directory_uri() . '/js/jquery-1.11.0.min.js', array(), '20151215' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- page-inicio.php
+++ page-inicio.php
@@ -7,6 +7,23 @@
 <?php
 get_header();
 ?>
+<?php
+$the_query = new WP_Query( array( 'posts_per_page' => 5, 'category_name' => 'Portada') );
+$counter = 0;
+/* Start the Loop */
+while ( $the_query->have_posts() ) :
+    $the_query->the_post();
+    if ($counter < 5) {
+        $imgs[] = get_the_post_thumbnail_url();
+        $links[] = get_the_permalink();
+        $titles[] = get_the_title();
+        $counter ++;
+    } 
+    else 
+        break;
+endwhile;
+wp_reset_postdata();
+?>
 <main class="el-main">
     <section class="<?php if ( is_front_page() ) : ?>container-home<?php else : ?>container page<?php endif; ?>">
         <div class="app-logo-container">
@@ -28,270 +45,41 @@ get_header();
         </div>  
     </section>
 </main>
-<?php
-$the_query = new WP_Query( array( 'posts_per_page' => 5, 'category_name' => 'Portada') );
-?>
-<ul class="cb-slideshow">
-<?php
-$counter = 0;
-/* Start the Loop */
-while ( $the_query->have_posts() ) :
-    $the_query->the_post();
-    if ($counter < 5) {
-        $imgs[] = get_the_post_thumbnail_url();
-        $titles[] = get_the_permalink();
+<div id="slider-news-title">
+    <a id="slider-news-title-anchor" href="<?php echo $links[0]; ?>"><?php echo $titles[0]; ?></a>
+</div>
+
+<div class="bullets">
+    <?php
+    for ($i=0; $i < $counter; $i++) { 
         ?>
-        <li>
-            <span></span>
-            <div>
-                <a href="<?php echo get_the_permalink(); ?>">
-                <?php
-                    echo get_the_title();
-                ?>
-                </a>
-            </div>
-        </li>
+        <span onClick="SetOpacity(<?php echo $i; ?>)">&#9679;</span>
         <?php
-        $counter ++;
-    } 
-    else 
-        break;
-endwhile;
-wp_reset_postdata();
-?>
-</ul>
-<style>
-
-/* General Demo Style */
-
-.cb-slideshow,
-.cb-slideshow:after { 
-    width: 100%;
-    height: 100%;
-    top: 0px;
-    left: 0px;
-    z-index: 0; 
-}
-.cb-slideshow:after { 
-    content: '';
-}
-.cb-slideshow li span { 
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    color: transparent;
-    background-size: cover;
-    background-position: 50% 50%;
-    background-repeat: none;
-    opacity: 0;
-    z-index: -10;
-	-webkit-backface-visibility: hidden;
-    -webkit-animation: imageAnimation 30s linear infinite 0s;
-    -moz-animation: imageAnimation 30s linear infinite 0s;
-    -o-animation: imageAnimation 30s linear infinite 0s;
-    -ms-animation: imageAnimation 30s linear infinite 0s;
-    animation: imageAnimation 30s linear infinite 0s; 
-}
-.cb-slideshow li div { 
-    position: absolute;
-    bottom: 50px;
-    left: 0px;
-    width: 100%;
-    text-align: center;
-    opacity: 0;
-    color: #fff;
-    -webkit-animation: titleAnimation 30s linear infinite 0s;
-    -moz-animation: titleAnimation 30s linear infinite 0s;
-    -o-animation: titleAnimation 30s linear infinite 0s;
-    -ms-animation: titleAnimation 30s linear infinite 0s;
-    animation: titleAnimation 30s linear infinite 0s; 
-}
-.cb-slideshow li div a { 
-    font-size: 2em;
-    margin-bottom: 50px;
-    line-height: 1em; 
-    text-decoration: none;
-    font-weight: 900;
-    color: white;
-    text-shadow: 0 0 10px black;
-}
-.cb-slideshow li:nth-child(1) span { 
-    background-image: url(<?php echo $imgs[0]; ?>);
-}
-.cb-slideshow li:nth-child(2) span { 
-    background-image: url(<?php echo $imgs[1]; ?>);
-    -webkit-animation-delay: 6s;
-    -moz-animation-delay: 6s;
-    -o-animation-delay: 6s;
-    -ms-animation-delay: 6s;
-    animation-delay: 6s; 
-}
-.cb-slideshow li:nth-child(3) span { 
-    background-image: url(<?php echo $imgs[2]; ?>);
-    -webkit-animation-delay: 12s;
-    -moz-animation-delay: 12s;
-    -o-animation-delay: 12s;
-    -ms-animation-delay: 12s;
-    animation-delay: 12s; 
-}
-.cb-slideshow li:nth-child(4) span { 
-    background-image: url(<?php echo $imgs[3]; ?>);
-    -webkit-animation-delay: 18s;
-    -moz-animation-delay: 18s;
-    -o-animation-delay: 18s;
-    -ms-animation-delay: 18s;
-    animation-delay: 18s; 
-}
-.cb-slideshow li:nth-child(5) span { 
-    background-image: url(<?php echo $imgs[4]; ?>);
-    -webkit-animation-delay: 24s;
-    -moz-animation-delay: 24s;
-    -o-animation-delay: 24s;
-    -ms-animation-delay: 24s;
-    animation-delay: 24s; 
-}
-.cb-slideshow li:nth-child(2) div { 
-    -webkit-animation-delay: 6s;
-    -moz-animation-delay: 6s;
-    -o-animation-delay: 6s;
-    -ms-animation-delay: 6s;
-    animation-delay: 6s; 
-}
-.cb-slideshow li:nth-child(3) div { 
-    -webkit-animation-delay: 12s;
-    -moz-animation-delay: 12s;
-    -o-animation-delay: 12s;
-    -ms-animation-delay: 12s;
-    animation-delay: 12s; 
-}
-.cb-slideshow li:nth-child(4) div { 
-    -webkit-animation-delay: 18s;
-    -moz-animation-delay: 18s;
-    -o-animation-delay: 18s;
-    -ms-animation-delay: 18s;
-    animation-delay: 18s; 
-}
-.cb-slideshow li:nth-child(5) div { 
-    -webkit-animation-delay: 24s;
-    -moz-animation-delay: 24s;
-    -o-animation-delay: 24s;
-    -ms-animation-delay: 24s;
-    animation-delay: 24s; 
-}
-/* Animation for the slideshow images */
-@-webkit-keyframes imageAnimation { 
-    0% { opacity: 0;
-    -webkit-animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         -webkit-animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-moz-keyframes imageAnimation { 
-    0% { opacity: 0;
-    -moz-animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         -moz-animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-o-keyframes imageAnimation { 
-    0% { opacity: 0;
-    -o-animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         -o-animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-ms-keyframes imageAnimation { 
-    0% { opacity: 0;
-    -ms-animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         -ms-animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@keyframes imageAnimation { 
-    0% { opacity: 0;
-    animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-/* Animation for the title */
-@-webkit-keyframes titleAnimation { 
-    0% { opacity: 0;
-    animation-timing-function: ease-in;  }
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-moz-keyframes titleAnimation { 
-    0% { opacity: 0 ;
-    animation-timing-function: ease-in;}
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-o-keyframes titleAnimation { 
-    0% { opacity: 0;
-    animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@-ms-keyframes titleAnimation { 
-    0% { opacity: 0;
-    animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-@keyframes titleAnimation { 
-    0% { opacity: 0;
-    animation-timing-function: ease-in; }
-    8% { opacity: 1;
-         animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-/* Show at least something when animations not supported */
-.no-cssanimations .cb-slideshow li span{
-	opacity: 1;
-}
-
-@media screen and (max-width: 1140px) { 
-    .cb-slideshow li div h3 { font-size: 140px }
-}
-@media screen and (max-width: 600px) { 
-    .cb-slideshow li div h3 { font-size: 80px }
-}
-</style>
+    }
+    ?>
+</div>
 <script>
-    var titles = ["<?php echo $titles[0]; ?>", "<?php echo $titles[1]; ?>", "<?php echo $titles[2]; ?>", "<?php echo $titles[3]; ?>", "<?php echo $titles[4]; ?>", ];
+    var imgUrls = ["<?php echo $imgs[0]; ?>", "<?php echo $imgs[1]; ?>", "<?php echo $imgs[2]; ?>", "<?php echo $imgs[3]; ?>", "<?php echo $imgs[4]; ?>", ];
+    var titles = ["<?php echo $links[0]; ?>", "<?php echo $links[1]; ?>", "<?php echo $links[2]; ?>", "<?php echo $links[3]; ?>", "<?php echo $links[4]; ?>", ];
+    var linkstext = ["<?php echo $titles[0]; ?>", "<?php echo $titles[1]; ?>", "<?php echo $titles[2]; ?>", "<?php echo $titles[3]; ?>", "<?php echo $titles[4]; ?>", ];
     var counter = 0;
-setInterval(function() {
-    var elem = document.querySelectorAll("ul.cb-slideshow a");
-    counter++;
-    elem[elem.length - 1].href = titles[counter % titles.length];
-  }, 6000);
+    var slider = document.getElementById("__nuxt");
+    var newslink = document.getElementById("slider-news-title-anchor");
+    $(slider).css({'background-image': "url('" + imgUrls[0] + "')", 'background-size': 'cover', 'background-position': 'center'});
+    function MyFunc() {
+        $(slider).css({'background-image': "url('" + imgUrls[counter % 5] + "')", 'background-size': 'cover', 'background-position': 'center'});
+        $(newslink).attr('href', titles[counter % 5]);
+        $(newslink).text(linkstext[counter % 5]);
+        counter++
+    }
+    var myTimer = setInterval(MyFunc, 6000);
+    function SetOpacity(position) {
+        $(slider).css({'background-image': "url('" + imgUrls[position] + "')", 'background-size': 'cover', 'background-position': 'center'});
+        $(newslink).attr('href', titles[position]);
+        $(newslink).text(linkstext[position]);
+        clearInterval(myTimer);
+        myTimer = setInterval(MyFunc, 6000);
+    }
 </script>
 <?php
 get_footer();
