b499bfd59483cddfdee1e3f61da7769dd20dbc5d
Pancho <pancho@localhost.localdomain>
2019-07-15 22:53:20 -0400

    Changed the play icon for a square

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -31,7 +31,7 @@ $cat_posts = get_posts(array('category' => $cat_id));
 						<li>
 							<a href="<?php echo get_permalink(get_the_ID()) . "?subpId=" . $value->ID; ?>">
 								<span>
-									<i class="glyphicon glyphicon-play">&#9654;</i>
+									<i class="glyphicon glyphicon-play">&#9632;</i>
 								</span> <?php echo $value->post_title; ?>
 							</a>
 						</li>
